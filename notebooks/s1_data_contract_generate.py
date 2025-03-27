5197130881430f8ba798ce5b45704c5ae2c10d50
robert-altmiller <robert-altmiller>
2025-03-27 04:21:27 +0000

    changes

--- notebooks/s1_data_contract_generate.py
+++ notebooks/s1_data_contract_generate.py
@@ -103,7 +103,9 @@ def create_local_data(catalog, schema, uc_tables_list, folder_path, method="csv"
     for table in uc_tables_list:
         file_name = f"{table}"
         if is_running_in_databricks_workflow() == True: 
+            print("in here")
             dbutils.fs.mkdirs(folder_path)
+            folder_path = folder_path.replace("/dbfs", "")
         os.makedirs(folder_path, exist_ok=True)
 
         df = spark.read.table(f"{catalog}.{schema}.{table}").limit(5)
