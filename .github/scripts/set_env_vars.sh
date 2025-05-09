a9bf04ff443bd99aa9fc2ebb45cfdd94a5af155f
Alan Reese <acreese11@gmail.com>
2025-05-09 18:07:03 -0500

    Refactor environment variable assignments in set_env_vars.sh for clarity and consistency in GitHub Actions workflow

--- .github/scripts/set_env_vars.sh
+++ .github/scripts/set_env_vars.sh
@@ -1,15 +1,15 @@
 # Get Databricks host url
-DATABRICKS_HOST="${{ github.event.inputs.DATABRICKS_HOST != '' && github.event.inputs.DATABRICKS_HOST || secrets.DATABRICKS_HOST }}"
+DATABRICKS_HOST="$DATABRICKS_HOST"
 echo "::add-mask::$DATABRICKS_HOST"
 echo "DATABRICKS_HOST: $DATABRICKS_HOST"
 
 # Get Databricks personal access token (PAT)
-DATABRICKS_TOKEN="${{ github.event.inputs.DATABRICKS_TOKEN != '' && github.event.inputs.DATABRICKS_TOKEN || secrets.DATABRICKS_TOKEN }}"
+DATABRICKS_TOKEN="$DATABRICKS_TOKEN"
 echo "::add-mask::$DATABRICKS_TOKEN"
 echo "DATABRICKS_TOKEN: $DATABRICKS_TOKEN"
 
 # Get target environment for DABs
-TARGET="${{ github.event.inputs.ENVIRONMENT || 'dev' }}"
+TARGET="$TARGET"
 echo "TARGET: $TARGET"
 
 # Get branch name
@@ -17,16 +17,16 @@ export BRANCH_NAME="$GITHUB_REF_NAME"
 echo "BRANCH_NAME: $BRANCH_NAME"
 
 # Get email
-USER_EMAIL="${{ github.event.inputs.USER_EMAIL || 'robert.altmiller@databricks.com' }}"
+USER_EMAIL="$USER_EMAIL"
 echo "::add-mask::$USER_EMAIL"
 echo "USER_EMAIL: $USER_EMAIL"
 
 # Get ODCS contract source catalog
-ODCS_CONTRACT_SOURCE_CATALOG="${{ github.event.inputs.ODCS_CONTRACT_SOURCE_CATALOG || 'hive_metastore' }}"
+ODCS_CONTRACT_SOURCE_CATALOG="$ODCS_CONTRACT_SOURCE_CATALOG"
 echo "ODCS_CONTRACT_SOURCE_CATALOG: $ODCS_CONTRACT_SOURCE_CATALOG"
 
 # Get ODCS contract source schema
-ODCS_CONTRACT_SOURCE_SCHEMA="${{ github.event.inputs.ODCS_CONTRACT_SOURCE_SCHEMA || 'default' }}"
+ODCS_CONTRACT_SOURCE_SCHEMA="$ODCS_CONTRACT_SOURCE_SCHEMA"
 echo "ODCS_CONTRACT_SOURCE_SCHEMA: $ODCS_CONTRACT_SOURCE_SCHEMA"
 
 # Convert email to username
