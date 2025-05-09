7f7e4a752c629df1bb35a2f1feb3f3ddc7e8b43a
Alan Reese <acreese11@gmail.com>
2025-05-09 17:47:18 -0500

    Add script to set environment variables for Databricks deployment in GitHub Actions workflow

---
+++ .github/scripts/set_env_vars.sh
@@ -0,0 +1,55 @@
+# Get Databricks host url
+DATABRICKS_HOST="${{ github.event.inputs.DATABRICKS_HOST != '' && github.event.inputs.DATABRICKS_HOST || secrets.DATABRICKS_HOST }}"
+echo "::add-mask::$DATABRICKS_HOST"
+echo "DATABRICKS_HOST: $DATABRICKS_HOST"
+
+# Get Databricks personal access token (PAT)
+DATABRICKS_TOKEN="${{ github.event.inputs.DATABRICKS_TOKEN != '' && github.event.inputs.DATABRICKS_TOKEN || secrets.DATABRICKS_TOKEN }}"
+echo "::add-mask::$DATABRICKS_TOKEN"
+echo "DATABRICKS_TOKEN: $DATABRICKS_TOKEN"
+
+# Get target environment for DABs
+TARGET="${{ github.event.inputs.ENVIRONMENT || 'dev' }}"
+echo "TARGET: $TARGET"
+
+# Get branch name
+export BRANCH_NAME="$GITHUB_REF_NAME"
+echo "BRANCH_NAME: $BRANCH_NAME"
+
+# Get email
+USER_EMAIL="${{ github.event.inputs.USER_EMAIL || 'robert.altmiller@databricks.com' }}"
+echo "::add-mask::$USER_EMAIL"
+echo "USER_EMAIL: $USER_EMAIL"
+
+# Get ODCS contract source catalog
+ODCS_CONTRACT_SOURCE_CATALOG="${{ github.event.inputs.ODCS_CONTRACT_SOURCE_CATALOG || 'hive_metastore' }}"
+echo "ODCS_CONTRACT_SOURCE_CATALOG: $ODCS_CONTRACT_SOURCE_CATALOG"
+
+# Get ODCS contract source schema
+ODCS_CONTRACT_SOURCE_SCHEMA="${{ github.event.inputs.ODCS_CONTRACT_SOURCE_SCHEMA || 'default' }}"
+echo "ODCS_CONTRACT_SOURCE_SCHEMA: $ODCS_CONTRACT_SOURCE_SCHEMA"
+
+# Convert email to username
+USER_NAME=$(echo "$USER_EMAIL" | cut -d '@' -f 1 | tr '.' '_')
+echo "USER_NAME: $USER_NAME"
+
+# Optionally make them available to later steps
+echo "DATABRICKS_HOST=$DATABRICKS_HOST" >> $GITHUB_ENV
+echo "DATABRICKS_TOKEN=$DATABRICKS_TOKEN" >> $GITHUB_ENV
+echo "TARGET=$TARGET" >> $GITHUB_ENV
+echo "BRANCH_NAME=$BRANCH_NAME" >> $GITHUB_ENV
+echo "ODCS_CONTRACT_SOURCE_CATALOG=$ODCS_CONTRACT_SOURCE_CATALOG" >> $GITHUB_ENV
+echo "ODCS_CONTRACT_SOURCE_SCHEMA=$ODCS_CONTRACT_SOURCE_SCHEMA" >> $GITHUB_ENV
+echo "USER_EMAIL=$USER_EMAIL" >> $GITHUB_ENV
+echo "USER_NAME=$USER_NAME" >> $GITHUB_ENV
+echo "GIT_REPO_URL=https://github.com/${GITHUB_REPOSITORY}.git" >> $GITHUB_ENV
+
+if [[ "$GITHUB_ACTIONS" == "true" ]]; then
+echo "Running inside GitHub CI/CD"
+echo "GIT_SOURCE=gitHub" >> $GITHUB_ENV
+elif [[ "$GITLAB_CI" == "true" ]]; then
+echo "Running inside GitLab CI/CD"
+echo "GIT_SOURCE=gitLab" >> $GITHUB_ENV
+else
+echo "Not running inside GitHub or GitLab"
+fi
